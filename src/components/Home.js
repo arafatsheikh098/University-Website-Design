21bfe03ce51af85780f6b68d2e5fc7339d0b24cf
arafatsheikh098 <arafat.csecu@gmail.com>
2024-12-11 22:59:19 +0600

    added home.js and home.css

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../components/Home.css';
+import Logo from '../components/logo.png';
+const Home = () => {
+    return (
+        <section className="header">
+            <nav>
+                <a href="index.html">
+                <img src={Logo} alt="Logo" />
+                </a>
+                <div className="nav-links">
+                    <ul>
+                        <li><a href="">HOME</a></li>
+                        <li><a href="">ABOUT</a></li>
+                        <li><a href="">COURSE</a></li>
+                        <li><a href="">BLOG</a></li>
+                        <li><a href="">CONTACT</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="text-box">
+                <h1>World's Biggest University</h1>
+                <p>
+                    Making website is now one of the easiest thing in the world.
+                    You just need to learn Html, Css, Javascript and you are good to go
+                </p>
+                <a href="" className="hero-btn">Visit us to know more</a>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
